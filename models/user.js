8a7a110a627b37877541865353877c3cfd666032
Karina Titov <karinasul1995@gmail.com>
2020-06-16 01:28:17 -0500

    trying to post

--- models/user.js
+++ models/user.js
@@ -19,4 +19,4 @@ const UserSchema = new Schema({
     default: Date.now
   }
 });
-module.exports = UserSchema = mongoose.model("users", UserSchema);
+module.exports = User= mongoose.model("users", UserSchema);
--- server.js
+++ server.js
@@ -2,16 +2,32 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
+const bodyParser = require("body-parser");
+const passport = require("passport");
 const PORT = process.env.PORT || 3000;
 
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
+const users = require("./routes/api/index");
+
+
+// Bodyparser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./config/passport")(passport);
+// Routes
+app.use("/api/index", users);
 
 // Add routes, both API and view
 app.use(routes);
