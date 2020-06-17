a502feda4e1b7ecbfbe1af30db3fef775fd34b73
AshfoMon <ashfordmonte@gmail.com>
2020-06-16 20:28:35 -0500

    Added comments in passport.js for encryption key

--- config/passport.js
+++ config/passport.js
@@ -2,8 +2,11 @@ const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
 const mongoose = require("mongoose");
 const User = mongoose.model("users");
+// keys = require("./pathToKey");
+
 const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+// opts.secretOrKey = keys.secretOrKey;
 module.exports = passport => {
   passport.use(
     new JwtStrategy(opts, (jwt_payload, done) => {
