db1da7a9874af72bbf3c1852ae93823e1664a2d3
Lyyte <lyytefaridi@gmail.com>
2020-06-25 15:01:39 -0500

    added dashboards with respective private routes; added role to use model, added line of code to send token to frontend, updated menu to show proper buttons based on signed in info

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,11 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Home from './core/home'
 import Signup from './user/Signup'
 import Signin from './user/Signin'
+import PrivateRoute from './user/PrivateRoute'
+import Dashboard from './user/UserDashboard'
+import AdminRoute from './user/AdminRoute'
+import AdminDashboard from './user/AdminDashboard'
+
 
 const Routes = () => {
     return (
@@ -11,6 +16,8 @@ const Routes = () => {
             <Route path='/' exact component={Home} />
             <Route path='/signin' exact component={Signin} />
             <Route path='/signup' exact component={Signup} />
+            <PrivateRoute path='/user/dashboard' exact component={Dashboard} />
+            <AdminRoute path='/admin/dashboard' exact component={AdminDashboard} />
         </Switch>
         </BrowserRouter>
     );
