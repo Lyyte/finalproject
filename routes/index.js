b26d53b7f95cf9666dac4c6266f02693207878e6
Karina Titov <karinasul1995@gmail.com>
2020-07-09 21:59:14 -0500

    changed the route

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,8 @@ router.use("/api", apiRoutes);
 
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
-  res.sendFile(path.resolve((__dirname, 'public/index.html')));
+  res.sendFile(path.resolve((__dirname, '../build/index.html')));
 })
 
 module.exports = router;
 
-// 
